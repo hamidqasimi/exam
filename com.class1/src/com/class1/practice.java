e00c95996080d78b1f6e2f7a799ba659abad7c81
hamidqasimi <hamidqasimi7@gmail.com>
2021-04-09 16:34:52 -0400

    first push after long time

---
+++ com.class1/src/com/class1/practice.java
@@ -0,0 +1,31 @@
+package com.class1;
+
+public class practice {
+public static void main(String[]args) {
+
+	float num1=12.5f;
+	float num2=3.5f;
+	
+	float div=num1/num2;
+	
+	
+	System.out.println(div);
+	
+	int number1=13;
+	int number2=2;
+	double divOfIntegers=number1/number2;
+	System.out.println(divOfIntegers);
+	 double d=12;
+	 System.out.print(d);
+	 
+	
+	
+	
+	
+}
+	
+	
+	
+	
+	
+}
